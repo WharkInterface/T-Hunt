c4764bb484660c38c16123cf11fbae6c533856ce
WharkInterface <60129804+WharkInterface@users.noreply.github.com>
2020-02-25 15:28:02 +0100

    Update qdee.js

--- qdee.js
+++ qdee.js
@@ -2,10 +2,16 @@
     Projet : T-Hunt
     Détails : QDEE
     Auteur : Alexandre Pintrand / Grégoire Pean / Killiany Lecosseck
-    Version du : 28/01/2020
+    Version du : 25/02/2020
 */
 
+let vMotD = 0
+let vMotG = 0
 qdee.qdee_Init()
-qdee.qdee_init_colorSensor(qdee.colorSensorPort.port4)
-qdee.qdee_setMotorSpeed(100, 98)
-music.playTone(587, music.beat(BeatFraction.Whole))
+let vitesse = 50
+let puissance = 1
+basic.forever(function () {
+    vMotG = vitesse + (qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2) - qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) / Math.max(qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2), qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) * vitesse
+    vMotD = vitesse - (qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2) - qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) / Math.max(qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2), qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) * vitesse
+    qdee.qdee_setMotorSpeed(vMotG, vMotD)
+})
