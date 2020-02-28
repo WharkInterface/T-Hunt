18dc3c4d74d90b58ee755ba53455fb58291de40b
Alexandre Pintrand <60129804+WharkInterface@users.noreply.github.com>
2020-02-28 15:45:19 +0100

    Update qdee.js

--- qdee.js
+++ qdee.js
@@ -8,10 +8,13 @@
 let vMotD = 0
 let vMotG = 0
 qdee.qdee_Init()
+qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, -120, 1000)
 let vitesse = 50
 let puissance = 1
 basic.forever(function () {
     vMotG = vitesse + (qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2) - qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) / Math.max(qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2), qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) * vitesse
     vMotD = vitesse - (qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2) - qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) / Math.max(qdee.qdee_ultrasonic(qdee.ultrasonicPort.port2), qdee.qdee_ultrasonic(qdee.ultrasonicPort.port1)) * vitesse
     qdee.qdee_setMotorSpeed(vMotG, vMotD)
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 1, 120, 1000)
+    qdee.qdee_setBusServo(qdee.busServoPort.port10, 2, -120, 1000)
 })
